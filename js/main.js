10e7177300225767a8084ad961f6178469f0ab39
Omer Farooq <umerf2700@gmail.com>
2025-07-16 22:10:57 +0500

    i updated navigatio bar for all working htmls# Please enter the commit message for your changes. Lines starting

--- js/main.js
+++ js/main.js
@@ -400,3 +400,75 @@ document.querySelector('.dropdown-menu').addEventListener('mouseleave', function
     menu.style.display = 'none';
   });
 });
+const products = [
+	{ name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+	{ name: "Adrona Embellished", price: "₨13,000.00", image: "images/traditional-2.jpg" },
+	{ name: "Aizal butterfly dress", price: "₨10,000.00", image: "images/traditional-3.jpg" },
+	{ name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+    { name: "Amal's butterfly dress", price: "₨8,000.00", image: "images/traditional-2.jpg" },
+	// 👉 Add up to 40+ products here
+];
+
+// Render products into the container
+const container = document.getElementById('product-list');
+products.forEach((product, index) => {
+	const html = `
+		<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item product-item" style="display: ${index < 8 ? '' : 'none'}">
+			<div class="block2">
+				<div class="block2-pic hov-img0">
+					<img src="${product.image}" alt="${product.name}">
+					<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+						Quick View
+					</a>
+				</div>
+				<div class="block2-txt flex-w flex-t p-t-14">
+					<div class="block2-txt-child1 flex-col-l">
+						<a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">${product.name}</a>
+						<span class="stext-105 cl3">From ${product.price}</span>
+					</div>
+					<div class="block2-txt-child2 flex-r p-t-3">
+						<a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+							<img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
+							<img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	`;
+	container.insertAdjacentHTML('beforeend', html);
+});
+
+// Load More Button
+const loadMoreBtn = document.querySelector('.load-more-btn');
+let visibleCount = 8;
+
+loadMoreBtn.addEventListener('click', function () {
+	const items = document.querySelectorAll('.product-item');
+	const next = visibleCount + 8;
+	for (let i = visibleCount; i < next && i < items.length; i++) {
+		items[i].style.display = '';
+	}
+	visibleCount = next;
+	if (visibleCount >= items.length) {
+		loadMoreBtn.style.display = 'none';
+	}
+});
